162467bdd79cba0401e2e9d2e5a5874eb4499859
GaitanK <happielife007@gmail.com>
2024-06-21 01:58:43 +0530

    controller skeleton code

---
+++ src/controllers/contactController.js
@@ -0,0 +1,9 @@
+// controllers/contactController.js
+const contactService = require("../services/contactService");
+
+const identifyContact = async (req, res) => {
+};
+
+module.exports = {
+  identifyContact,
+};
