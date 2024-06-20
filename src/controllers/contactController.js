816fc8bf4a73d9a9d89446a1ba52830ec5da4d07
GaitanK <happielife007@gmail.com>
2024-06-21 01:59:12 +0530

    controller try catch handler

--- src/controllers/contactController.js
+++ src/controllers/contactController.js
@@ -2,6 +2,13 @@
 const contactService = require("../services/contactService");
 
 const identifyContact = async (req, res) => {
+  try {
+    const result = await contactService.identifyContact(req.body);
+    res.status(200).json({ contact: result });
+  } catch (error) {
+    console.log("Error in identifyContact: ", error)
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 module.exports = {
