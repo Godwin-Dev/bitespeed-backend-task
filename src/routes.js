867eb3ab8eaf9c62ef24b8c6cd51f398f438d05c
GaitanK <happielife007@gmail.com>
2024-06-21 01:59:47 +0530

    /identify route added

--- src/routes.js
+++ src/routes.js
@@ -3,5 +3,6 @@ const { Contact } = require("./models");
 const router = express.Router();
 const contactController = require('./controllers/contactController');
 
+router.post('/identify', contactController.identifyContact);
 
 module.exports = router;
